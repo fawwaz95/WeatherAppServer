affd1fc75e58af2f429317641f0cdb491f797b90
fawwaz95 <fawwaz_95@hotmail.com>
2023-05-06 20:32:00 -0400

    removed cors addedd access control to fix mobile fetching issue

--- app.js
+++ app.js
@@ -10,8 +10,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
+
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post('/CurrentWeather', (req, res) => {
   const {cityName} = req.body;
@@ -19,11 +30,21 @@ app.post('/CurrentWeather', (req, res) => {
   fetchingAPI.fetchAPI(apiUrl, 'weather', apikey, res);
 });
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.post('/FiveDayForecast', (req, res) => {
   const {cityName} = req.body;
   const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apikey}`;
   fetchingAPI.fetchAPI(apiUrl, 'forecast', apikey, res);
-})
+});
 
 app.listen(PORT, () =>{
   console.log(`App is running on port ${PORT}`);
